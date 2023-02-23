d2abb45fa8d3372ec89c4e134574f837af4e2751
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-02-23 16:35:35 +0100

    inseriti i dati in nome passeggiero, tipo di offerta

--- js/main.js
+++ js/main.js
@@ -1,27 +1,67 @@
+const under18 = "Minorenne"
+const adult = "Maggiorenne"
+const over65 = "Over65"
+
 const domButton = document.querySelector('#btn1');
 
-domButton.addEventListener('click', 
+domButton.addEventListener('click',
     function () {
         const nomeCognome = document.getElementById('nomeCognome');
         const nomeCognomeCompilato = nomeCognome.value;
-        
+        console.log(nomeCognomeCompilato);
+
         const kmViaggio = document.getElementById('kmViaggio');
         const kmViaggioCompilato = kmViaggio.value;
+        console.log(kmViaggioCompilato);
+
+        const numeroKm = kmViaggioCompilato;
+        const costoBase = (numeroKm * 0.21);
+        let costoEffettivo;
 
         const casellaSelezione = document.getElementById('casellaSelezione');
         const casellaSelezioneScelta = casellaSelezione.value;
+        console.log(casellaSelezioneScelta);
+
+
+        if (casellaSelezioneScelta == under18) {
+            costoEffettivo = (costoBase * 0.8).toFixed(2);
+
+        } else if (casellaSelezioneScelta == over65) {
+            costoEffettivo = (costoBase * 0.6).toFixed(2);
+
+        } else {
+            costoEffettivo = (costoBase).toFixed(2);
+
+        }
+
+        const nome = document.getElementById('nome');
+        nome.innerHTML = nomeCognomeCompilato;
+
+        const offerta = document.getElementById('offerta');
+        offerta.innerHTML = casellaSelezioneScelta;
+
+
+        const costo = document.getElementById('costo');
+        costo.innerHTML = costoEffettivo;
+
     }
 );
 
+
+
+
+
 const domReset = document.querySelector('#btn2');
 
-domReset.addEventListener('click', 
-    function() {
+domReset.addEventListener('click',
+    function () {
         nomeCognome.value = "";
 
         kmViaggio.value = "";
     }
 );
+
+
 
 
 
